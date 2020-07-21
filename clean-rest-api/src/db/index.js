e28814ca77c6eec6e1d94ebacab3812a6fb651c7
Suraj Karki <suraj.karki500@gmail.com>
2020-07-21 22:48:17 +0545

    Created new mongodb client

---
+++ clean-rest-api/src/db/index.js
@@ -0,0 +1,16 @@
+import mongodb from "mongodb";
+
+export default async function makeDb() {
+  const MongoClient = mongodb.MongoClient;
+  const url = "mongodb://localhost:27017";
+  const dbName = "clean-api";
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  await client.connect();
+  const db = await client.db(dbName);
+  db.makeId = makeIdFromString;
+  return db;
+}
+
+function makeIdFromString(id) {
+  return new mongodb.ObjectID(id);
+}
