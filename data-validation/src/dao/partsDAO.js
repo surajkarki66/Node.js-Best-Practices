7880eec10f4bc7bf6784075ab1d47146ceeb0c72
Suraj Karki <suraj.karki500@gmail.com>
2020-09-03 17:07:26 +0545

    Finished partsDAO

--- data-validation/src/dao/partsDAO.js
+++ data-validation/src/dao/partsDAO.js
@@ -1,3 +1,5 @@
+import logger from "../utils/logger";
+
 let parts;
 const DEFAULT_SORT = [["name", -1]];
 export default class PartsDAO {
@@ -7,19 +9,26 @@ export default class PartsDAO {
     }
     try {
       parts = await conn.db(process.env.NS).collection("parts");
+      logger.info(
+        `Connected to parts collection of ${process.env.NS} database.`,
+        "PartsDAO.injectDB()"
+      );
     } catch (e) {
-      console.error(`Unable to establish collection handles in partDAO: ${e}`);
+      logger.error(
+        `Error while injecting DB: ${e.message}`,
+        "PartsDAO.injectDB()"
+      );
+      throw e;
     }
   }
 
   static async create(partInfo) {
     try {
-      const data = await parts.insertOne(partInfo);
-      const part = data.ops[0];
-      return { success: true, part };
+      const result = await parts.insertOne(partInfo);
+      return { data: { createdId: result.insertedId }, statusCode: 201 };
     } catch (e) {
-      console.error(e.message);
-      return;
+      logger.error("Error occurred: " + e.message, "create()");
+      throw e;
     }
   }
 
@@ -30,22 +39,30 @@ export default class PartsDAO {
     try {
       cursor = await parts.find(query).project(project).sort(sort);
     } catch (e) {
-      console.error(`Unable to issue find command, ${e}`);
-      return { partsList: [], totalNumParts: 0 };
+      logger.error(`Unable to issue find command, ${e.message}`);
+      return {
+        data: [],
+        totalNumParts: 0,
+        statusCode: 404,
+      };
     }
     const displayCursor = cursor
       .skip(parseInt(page) * parseInt(partsPerPage))
       .limit(parseInt(partsPerPage));
     try {
-      const partsList = await displayCursor.toArray();
+      const documents = await displayCursor.toArray();
       const totalNumParts =
         parseInt(page) === 0 ? await parts.countDocuments(query) : 0;
-      return { partsList, totalNumParts };
+      return {
+        data: documents,
+        totalNumParts,
+        statusCode: documents.length > 0 ? 200 : 404,
+      };
     } catch (e) {
-      console.error(
-        `Unable to convert cursor to array or problem counting documents, ${e}`
+      logger.error(
+        `Unable to convert cursor to array or problem counting documents, ${e.message}`
       );
-      return { partsList: [], totalNumParts: 0 };
+      throw e;
     }
   }
 }
