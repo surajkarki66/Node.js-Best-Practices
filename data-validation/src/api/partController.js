96b9ff1b85070b70b68420e24276315d7b9fc161
Suraj Karki <suraj.karki500@gmail.com>
2020-09-03 17:23:01 +0545

    Finished partController

--- data-validation/src/api/partController.js
+++ data-validation/src/api/partController.js
@@ -1,12 +1,13 @@
 import PartsDAO from "../dao/partsDAO";
 import ApiError from "../error/ApiError";
+import writeServerJsonResponse from "../utils/utils";
 export default class PartController {
   static async addPart(req, res, next) {
     try {
       const part = req.body;
-      const data = await PartsDAO.create(part);
-      if (data.success) {
-        res.status(201).json(data);
+      const result = await PartsDAO.create(part);
+      if (result) {
+        writeServerJsonResponse(res, result.data, result.statusCode);
       }
     } catch (e) {
       next(ApiError.internal(`Something went wrong: ${e.message}`));
@@ -16,18 +17,18 @@ export default class PartController {
   static async listPart(req, res) {
     try {
       const { page, partsPerPage } = req.query;
-      const { partsList, totalNumParts } = await PartsDAO.getParts({
+      const result = await PartsDAO.getParts({
         page,
         partsPerPage,
       });
-      const response = {
-        parts: partsList,
+      const part = {
+        parts: result.data,
         page: page,
         filters: {},
         entries_per_page: partsPerPage,
-        total_results: totalNumParts,
+        total_results: result.totalNumParts,
       };
-      res.status(200).json(response);
+      writeServerJsonResponse(res, part, result.statusCode);
     } catch (e) {
       next(ApiError.internal(`Something went wrong: ${e.message}`));
       return;
