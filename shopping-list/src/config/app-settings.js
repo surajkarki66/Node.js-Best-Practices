5b00cca883d9dea9de5c3463aec0c35f2f0b84c5
Suraj Karki <suraj.karki500@gmail.com>
2020-09-03 13:10:22 +0545

    Finished load-mongodb

--- shopping-list/src/config/app-settings.js
+++ shopping-list/src/config/app-settings.js
@@ -4,8 +4,8 @@ const appSettings = {
   mongodb_url:
     "mongodb+srv://michaelkarki:kPmFlFoN4uRgQObP@cluster0.ee1zn.mongodb.net?retryWrites=true&w=majority",
   mongodb_db_name: "shoppingList",
-  brand_file_name: "../../dataset/Grocery_Brands_Database.csv",
-  item_file_name: "../../dataset/Grocery_UPC_Database.csv",
+  brand_file_name: "./dataset/Grocery_Brands_Database.csv",
+  item_file_name: "./dataset/Grocery_UPC_Database.csv",
   server_host: "localhost",
   server_listen_port: 5000,
 };
