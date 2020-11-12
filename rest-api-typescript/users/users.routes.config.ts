c76cb0c4983cb4d5b69a84c92c0eafdf2f196464
Suraj Karki <suraj.karki500@gmail.com>
2020-11-12 16:39:09 +0545

    Added some routes in configroutes method

--- rest-api-typescript/users/users.routes.config.ts
+++ rest-api-typescript/users/users.routes.config.ts
@@ -6,4 +6,43 @@ export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
     super(app, "UsersRoutes");
   }
+  configureRoutes() {
+    this.app
+      .route(`/users`)
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`List of users`);
+      })
+      .post((req: express.Request, res: express.Response) => {
+        res.status(200).send(`Post to users`);
+      });
+
+    this.app
+      .route(`/users/:userId`)
+      .all(
+        (
+          req: express.Request,
+          res: express.Response,
+          next: express.NextFunction
+        ) => {
+          // this middleware function runs before any request to /users/:userId
+          // but it doesn't accomplish anything just yet---
+          // it simply passes control to the next applicable function below using next()
+          next();
+        }
+      )
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.userId}`);
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.userId}`);
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+      });
+
+    return this.app;
+  }
 }
